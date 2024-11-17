527d71e1c93a6ca4fbaa6cb6740855ad7b2c122a
constantineIlario <costailario@Outlook.com>
2024-11-17 20:15:17 +0000

    finished

--- solitaire-part-2/src/main/java/solitaire/GameState.java
+++ solitaire-part-2/src/main/java/solitaire/GameState.java
@@ -31,7 +31,11 @@ public class GameState {
         //to create a new deck, you need to create all possible cards and push them onto the deck stack. 
         //Take a look at the Card.java file to see how cards are created
         //I recommend that you take a look at this resource, especially the 'iterate using for loop section': https://www.geeksforgeeks.org/iterating-over-enum-values-in-java/
-
+        for (Suit suit : Suit.values()) { // Iterate over each suit
+            for (Rank rank : Rank.values()) { 
+                deck.push(new Card(suit, rank)); 
+            }
+        }
 
     }
 
@@ -47,18 +51,34 @@ public class GameState {
         //to take a new card, you want to pop it from the deck stack
         //remember, each pile has a different ammount of cards, ranging 1-7
         //note - only top card of each pile should be faceup
-
+        for (int i = 0; i < gamePiles.length; i++) { // Loop over each game pile
+            for (int j = 0; j <= i; j++) { // Deal increasing number of cards (1 to 7)
+                Card card = deck.pop();
+                if (j == i) { // Top card in the pile
+                    card.flip(); // Flip it to face up
+                }
+                gamePiles[i].push(card);
+            }
+        }
 
     }
 
     // NEED TO IMPLEMENT
     public void drawFromDeck() {
         //this function should draw the top 3 cards from the deck, and add them to visibleCards
+        for (int i = 0; i < 3 && !deck.isEmpty(); i++) {
+            Card card = deck.pop();
+            card.flip(); // Make the drawn card visible
+            visibleCards.push(card);
+        }
     }
 
     // NEED TO IMPLEMENT
     public void discardCards() {
         //takes whatever cards are remaining in the visibleCards pile and moves them to the discardPiles
+        while (!visibleCards.isEmpty()) {
+            discardedCards.push(visibleCards.pop());
+        }
     }
 
     //Don't change this, used for testing
